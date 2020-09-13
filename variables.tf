3c17e24017b2a7b6a88df56d15ccea509bbf1b45
ayu004 <34073010+ayu004@users.noreply.github.com>
2020-09-14 04:06:56 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "project" {
+  description = "Project ID"
+  default     = "codecomp3-<team-name>"
+}
+
+variable "region" {
+  default = "us-central1"
+
+}
+
+variable "zone" {
+  default = "us-central1-f"
+}
+
+variable "credentials_file_path" {
+  default = "service-account.json"
+}
+
+variable "service_list" {
+    type = list
+    default = [
+	"cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "logging.googleapis.com",
+    "iam.googleapis.com",
+    "admin.googleapis.com",
+    "appengine.googleapis.com",
+    "storage-api.googleapis.com",
+    "cloudbuild.googleapis.com",
+  ]
+}
