2cc8b9eda9ea919f29f03d7ec4494845ee59e2ae
ayu004 <34073010+ayu004@users.noreply.github.com>
2020-09-15 22:42:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,17 @@
-provider "google" {
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
-}
-
 terraform {
-  required_version = ">= 0.12.6"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+  
   backend "gcs" {
-    bucket = "codecomp3-<team-name>"
+    bucket  = "codecomp3-champ"
+    prefix  = "terraform/state"
   }
 }
 
-resource "google_service_account" "service_account" {
-  account_id   = var.service_account_id
-  display_name = "Terraform Service Account"
-}
-
-resource "google_project_iam_binding" "project" {
-  project = "your-project-id"
-  role    = "roles/appengine.appAdmin"
-
-  members = ["service-account mail id",]
-}
-
-resource "google_project_service" "project" {
-  project = var.project
-  count   = length(var.service_list)
-  service = var.service_list[count.index]
-
-  disable_dependent_services = true
+provider "google" {
+ project     = "gcp-terraform-289514"
+ region      = "us-west1"
 }
