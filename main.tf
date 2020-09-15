946e896099cf3998c2449a84dbc41cbac2430df9
ayu004 <34073010+ayu004@users.noreply.github.com>
2020-09-16 01:01:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ provider "google" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
-
+  zone = "us-west1-a"
+  
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
