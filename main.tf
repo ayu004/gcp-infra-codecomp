14b4a6d81a70c3e05ed2f4ff8d1408f861cd5965
ayu004 <34073010+ayu004@users.noreply.github.com>
2020-09-16 00:36:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ terraform {
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
 provider "google" {
+  credentials = var.credentials
   project = "gcp-terraform-289514"
   region  = "us-west1"
 }
