1d8d98389349d19eccb68475776f4c1e9bbdaf24
ayu004 <34073010+ayu004@users.noreply.github.com>
2020-09-16 01:12:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,26 +16,17 @@ provider "google" {
   region  = "us-west1"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-  zone = "us-west1-a"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = "3"
+    }
+    action {
+      type = "Delete"
     }
   }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-    }
-  }
-}
-
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
 }
